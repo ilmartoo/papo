cf53d48b33ca90b887a91c598f5ad2db1f16906e
TheMartn <martinszga@gmail.com>
2021-07-29 19:35:53 +0200

    Properly detect all-caps words

--- papo.py
+++ papo.py
@@ -12,18 +12,22 @@ def load(f: str or object) -> str:
         raise TypeError('Input must be an existing text readable file or a path to an existing readable file')
 
     text = file.read()
-    papo = re.sub(r'(^|\s)([Pp]apo){0,1}(\w)', _capitalize_match, text)
+    papo = re.sub(r'(^|\s)(((Papo|papo|PAPO){1}\w*)|(\w+))', _capitalize_match, text)
 
     return papo
 
 def _capitalize_match(m: re.Match) -> str:
-    if m.group(3).istitle():
-        papo = f'Papo{m.group(3).lower()}'
-    elif m.group(3).isupper():
-        papo = f'PAPO{m.group(3)}'
+    if m.group(3):
+        return f'{m.group(1)}{m.group(3)}'
     else:
-        papo = f'papo{m.group(3)}'
-    return f'{m.group(1)}{papo}'
+        text = m.group(5)
+        if text.istitle():
+            papo = f'Papo{text.lower()}'
+        elif text.isupper():
+            papo = f'PAPO{text}'
+        else:
+            papo = f'papo{text}'
+        return f'{m.group(1)}{papo}'
 
 def _is_file(f: object) -> bool:
     return isinstance(f, io.TextIOWrapper) if sys.version_info[0] == 2 else hasattr(f, 'read') and hasattr(f, 'seek')
